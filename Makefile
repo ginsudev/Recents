4446a114f08e0e17e146023a84bda1e340187911
ginsu <noahlittle199@gmail.com>
2022-01-08 19:05:06 +1100

    v1.1.4

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ ARCHS = arm64 arm64e
 THEOS_DEVICE_IP = 192.168.1.106 #localhost -p 2222
 INSTALL_TARGET_PROCESSES = SpringBoard
 TARGET = iphone:clang:14.4:13
-PACKAGE_VERSION = 1.1.3
+PACKAGE_VERSION = 1.1.4
 
 include $(THEOS)/makefiles/common.mk
 
--- Sources/Recents/RCNTSCollectionViewCell.swift
+++ Sources/Recents/RCNTSCollectionViewCell.swift
@@ -14,9 +14,9 @@ class RCNTSCollectionViewCell: UICollectionViewCell {
     override init(frame: CGRect) {
         super.init(frame: frame)
         
-        let iconSize = frame.width
-        icon = UIImageView(frame: CGRect(x: 0,
-                                         y: 0,
+        let iconSize = frame.width - 22
+        icon = UIImageView(frame: CGRect(x: frame.width/2 - iconSize/2,
+                                         y: frame.height/2 - iconSize/2,
                                          width: iconSize,
                                          height: iconSize))
         icon.contentMode = .scaleAspectFit
